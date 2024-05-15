6583da75d76a36c49aef220a0d978efa5c3bf4bd
Leonardo <leo.3398@hotmail.com>
2024-05-15 18:33:36 -0300

    First commit

---
+++ aula 10 - Recursos Nativos do Navegador/index.js
@@ -0,0 +1,7 @@
+let nome = "Leonardo"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+window.alert("Pega no meu pau")
---
+++ aula 9 - integrando JS no html/index.js
@@ -0,0 +1,5 @@
+let name = "Leonardo"
+let x = 22
+
+console.log(name)
+console.log(x)
